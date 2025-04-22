1c9cb7c9dadca8ba015bd183a306b078e009b0bb
Yura Soroka <ykotov18@gmail.com>
2025-04-22 12:37:01 +0300

    add constants

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,14 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const DISCOUNT_FOR_SEVEN_PLUS_DAYS = 50;
+  const DISCOUNT_FOR_THREE_TO_SEVEN_DAYS = 20;
   let totalAmount = 0;
 
   if (days >= 7) {
-    totalAmount = -50;
+    totalAmount = -DISCOUNT_FOR_SEVEN_PLUS_DAYS;
   }
 
   if (days >= 3 && days < 7) {
-    totalAmount = -20;
+    totalAmount = -DISCOUNT_FOR_THREE_TO_SEVEN_DAYS;
   }
 
   for (let i = 0; i < days; i++) {
